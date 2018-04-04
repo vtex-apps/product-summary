70f3f0cff1bcd6a9811c5b4c0746aa6249df7aca
Vitória Heliane <vitoriaheliane@gmail.com>
2018-04-04 17:42:15 -0300

    Add JSDoc and propTypes

--- react/DiscountBadge.js
+++ react/DiscountBadge.js
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
 import {FormattedNumber} from 'react-intl';
+import PropTypes from 'prop-types';
 
+/**
+* The discount badge component. It recieves the product's list and selling prices
+* and calculates the discount percent to show it in the product's sumary.
+*/
 class DiscountBadge extends Component {
-
-  calculatePercentage() {
-    return (this.props.listPrice - this.props.sellingPrice) / this.props.listPrice;
+  /**
+  * This method calculates the discount tax by the selling and list prices.
+  */
+  calculateDiscountTax() {
+    const listPrice = this.props.listPrice;
+    const sellingPrice = this.props.sellingPrice;
+    return (listPrice - sellingPrice) / listPrice;
   }
 
   render() {
-    const percent = this.calculatePercentage();
-    return (percent) &&
+    const percent = this.calculateDiscountTax();
+    return (percent !== 0) &&
       <div className="f7 dark-gray absolute right-0 pa2-s bg-white">
         <FormattedNumber value={percent} style="percent"/> OFF
       </div>;
   }
 }
+
+DiscountBadge.propTypes = {
+  /** The product's default price */
+  listPrice: PropTypes.number.isRequired,
+  /** The product's price with discount */
+  sellingPrice: PropTypes.number.isRequired
+};
 
 export default DiscountBadge;
