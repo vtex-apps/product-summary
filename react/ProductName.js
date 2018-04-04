926b57f77fd2734565d2c66bc4e33fae34a668d1
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-04-04 13:42:12 -0300

    Move proptypes out of the component

--- react/ProductName.js
+++ react/ProductName.js
@@ -5,20 +5,6 @@ import PropTypes from 'prop-types'
  * Name component. Show name and relevant SKU information of the Product Summary
  */
 class ProductName extends Component {
-  /**
-   * @type {Object}
-   * @property {string} name - The name of the product
-   * @property {string} url - The url to the product main page
-   * @property {string} skuVariation - The name of the selected SKU
-   * @property {string} brandName - Name of the brand of the product
-   */
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    skuVariation: PropTypes.string,
-    brandName: PropTypes.string
-  }
-
   render() {
     const { name, skuVariation, brandName, url } = this.props
 
@@ -28,6 +14,20 @@ class ProductName extends Component {
       </a>
     )
   }
+}
+
+/**
+ * @type {Object}
+ * @property {string} name - The name of the product
+ * @property {string} url - The url to the product main page
+ * @property {string} skuVariation - The name of the selected SKU
+ * @property {string} brandName - Name of the brand of the product
+ */
+ProductName.propTypes = {
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  skuVariation: PropTypes.string,
+  brandName: PropTypes.string
 }
 
 export default ProductName
