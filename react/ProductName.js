bd74f212e04fc8c082298abe18be7e2c096ca21b
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-04-04 11:25:16 -0300

    Add jsdoc to proptypes and component

--- react/ProductName.js
+++ react/ProductName.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+/**
+ * Name component. Show name and relevant SKU information of the Product Summary
+ */
 class ProductName extends Component {
+  /**
+   * @type {Object}
+   * @property {string} name - The name of the product
+   * @property {string} url - The url to the product main page
+   * @property {string} skuVariation - The name of the selected SKU
+   * @property {string} brandName - Name of the brand of the product
+   */
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    skuVariation: PropTypes.string,
+    brandName: PropTypes.string
+  }
+
   render() {
     const { name, skuVariation, brandName, url } = this.props
 
@@ -11,13 +28,6 @@ class ProductName extends Component {
       </a>
     )
   }
-}
-
-ProductName.propTypes = {
-  name: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-  skuVariation: PropTypes.string,
-  brandName: PropTypes.string
 }
 
 export default ProductName
