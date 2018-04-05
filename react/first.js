a979a6a15d02cea2bf40dd33a5b48e5ddbbe926c
Vitória Heliane <vitoriaheliane@gmail.com>
2018-04-05 14:47:17 -0300

    Add mock with json instead of constant values

--- react/first.js
+++ react/first.js
@@ -2,10 +2,15 @@ import React from 'react'
 import {FormattedMessage, FormattedHTMLMessage} from 'react-intl'
 import DiscountBadge from './DiscountBadge'
 
+const product = {
+  listPrice: 200,
+  sellingPrice: 150,
+}
+
 const FirstStep = () =>
   <div>
     <div className="relative dib">
-      <DiscountBadge listPrice={200} sellingPrice={20} />
+      <DiscountBadge listPrice={product.listPrice} sellingPrice={product.sellingPrice} />
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd0cHfPOO0tneOT0AH3UDs7BumkdOVHZtv4DL55dFtInS2q8mi"/>
     </div>
     <h2>
