839a600241d220a1f69795024c4d7aa5c317f759
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-04-17 17:22:10 -0300

    Add Thumbnails tests

--- react/__tests__/ProductImage.test.js
+++ react/__tests__/ProductImage.test.js
@@ -10,9 +10,20 @@ describe('<ProductImage /> component', () => {
       {
         imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
         imageText: ""
+      },
+      {
+        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
+        imageText: ""
+      },
+      {
+        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
+        imageText: ""
+      },
+      {
+        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
+        imageText: ""
       }
-    ],
-    orientation: "VERTICAL"
+    ]
   }
 
   const defaultConfiguration = {}
@@ -35,8 +46,25 @@ describe('<ProductImage /> component', () => {
     }
   }
 
-  it('should be mounted properly without breaking', () => {
+  it('should be mounted without breaking', () => {
     const { component } = renderComponent()
     expect(component).toBeTruthy()
   })
+
+  describe('Thumbnails', () => {
+    it('should hide thumbnails if the number of images is not greater than one', () => {
+      const { component } = renderComponent()
+      // TODO
+      // expect(
+      //   component.find('.slick-list')
+      // ).toBe(false)
+    })
+  
+    it('should display at most 4 thumbnail items at same time', () => {
+      const { component } = renderComponent()
+      expect(
+        component.find('.slick-active').length
+      ).toBeLessThanOrEqual(4)
+    })
+  }) 
 })
