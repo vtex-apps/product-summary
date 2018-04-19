c375b782fea9b24d7337b544a09c520d4f858f0a
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-04-19 15:35:41 -0300

    Add tests for Thumbnail Slider component

--- react/__tests__/ProductImage.test.js
+++ react/__tests__/ProductImage.test.js
@@ -5,27 +5,7 @@ import { getIntlContextInfo, getIntlInstance } from './helpers/intlHelper'
 import ProductImage from '../ProductImage'
 
 describe('<ProductImage /> component', () => {
-  const productImagePropsMock = {
-    images: [
-      {
-        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
-        imageText: ""
-      },
-      {
-        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
-        imageText: ""
-      },
-      {
-        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
-        imageText: ""
-      },
-      {
-        imageUrl: "https://www.pontofrio-imagens.com.br/Informatica/Notebook/11483028/816389617/notebook-acer-core-i5-7200u-8gb-1tb-tela-15-6-windows-10-aspire-f5-573-51lj-11483028.jpg",
-        imageText: ""
-      }
-    ]
-  }
-
+  const productImagePropsMock = {}
   const defaultConfiguration = {}
 
   function renderComponent(customProps, intlInfo = getIntlContextInfo()) {
@@ -48,23 +28,51 @@ describe('<ProductImage /> component', () => {
 
   it('should be mounted without breaking', () => {
     const { component } = renderComponent()
-    expect(component).toBeTruthy()
+    expect(
+      component
+    ).toBeTruthy()
   })
 
-  describe('Thumbnails', () => {
-    it('should hide thumbnails if the number of images is not greater than one', () => {
-      const { component } = renderComponent()
-      // TODO
-      // expect(
-      //   component.find('.slick-list')
-      // ).toBe(false)
+  it('should hide thumbnail slider if the number of images is not greater than one', () => {
+    const { component } = renderComponent({
+      images: [ { imageUrl: "https://raw.githubusercontent.com/vtex-apps/product-summary/feature/product-image-tests/images/imagem-prod-summary10.png", imageText: "" } ]
     })
-  
-    it('should display at most 4 thumbnail items at same time', () => {
+    expect(
+      component.find('.slick-list')
+    ).toBeUndefined
+  })
+
+  describe('<ThumbnailSlider /> component', () => {  
+    const MAX_VISIBLE_ITEMS = 4
+    const NUM_OF_VISIBLE_ITEMS = 2
+    const INVALID_NUM_OF_VISIBLE_ITEMS = 10
+
+    it('should display at most MAX_VISIBLE_ITEMS thumbnail items at same time', () => {
       const { component } = renderComponent()
       expect(
         component.find('.slick-active').length
-      ).toBeLessThanOrEqual(4)
+      ).toBeLessThanOrEqual(MAX_VISIBLE_ITEMS)
+    })
+
+    it('should display NUM_OF_VISIBLE_ITEMS thumbnail items that is passed as a props, since that this number is less than or qual to MAX_VISIBLE_ITEMS', () => {
+      const { component } = renderComponent({
+        thumbnailMaxVisibleItems: NUM_OF_VISIBLE_ITEMS
+      })
+      expect(
+        component.find('.slick-active').length
+      ).toBe(NUM_OF_VISIBLE_ITEMS)
+    })
+
+    it('should display the entire image of thumbnails if this number is less than the NUM_OF_VISIBLE_ITEMS that is passed as a props', () => {
+      const images = [ { imageUrl: "https://raw.githubusercontent.com/vtex-apps/product-summary/feature/product-image-tests/images/imagem-prod-summary10.png", imageText: "" },
+                       { imageUrl: "https://raw.githubusercontent.com/vtex-apps/product-summary/feature/product-image-tests/images/imagem-prod-summary10.png", imageText: "" } ]
+      const { component } = renderComponent({
+        images: images,
+        thumbnailMaxVisibleItems: INVALID_NUM_OF_VISIBLE_ITEMS
+      })
+      expect(
+        component.find('.slick-active').length
+      ).toBe(images.length)
     })
   }) 
 })
