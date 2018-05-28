8bf6e47d817cc3dacfe8b540c18e565cb4978fd4
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-28 15:31:47 -0300

    Refact PropTypes

--- react/ProductSummary.js
+++ react/ProductSummary.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import { isMobile } from 'react-device-detect'
 import { Link } from 'render'
@@ -10,6 +9,7 @@ import ProductPrice from 'vtex.store-components/ProductPrice'
 import DiscountBadge from 'vtex.store-components/DiscountBadge'
 
 import { createProduct } from './ProductFactory'
+import ProductSummaryPropTypes from './propTypes'
 
 import './global.css'
 
@@ -17,6 +17,17 @@ import './global.css'
  * Product Summary component. Summarizes the product information.
  */
 class ProductSummary extends Component {
+  static propTypes = ProductSummaryPropTypes
+
+  static defaultProps = {
+    showListPrice: true,
+    showInstallments: true,
+    showLabels: true,
+    showBadge: true,
+    hideBuyButton: false,
+    showOnHover: false,
+  }
+
   constructor(props) {
     super(props)
     this.state = { isHovering: false }
@@ -105,82 +116,6 @@ class ProductSummary extends Component {
       </div>
     )
   }
-}
-
-ProductSummary.propTypes = {
-  /** Product that owns the informations */
-  product: PropTypes.shape({
-    /** Product's link text */
-    linkText: PropTypes.string.isRequired,
-    /** Product's name */
-    productName: PropTypes.string.isRequired,
-    /** Product's brand */
-    brand: PropTypes.string.isRequired,
-    /** Product's SKU */
-    sku: PropTypes.shape({
-      /** SKU name */
-      name: PropTypes.string.isRequired,
-      /** SKU id */
-      itemId: PropTypes.string.isRequired,
-      /** SKU Image to be shown */
-      image: PropTypes.shape({
-        /** Image URL */
-        imageUrl: PropTypes.string.isRequired,
-        /** Image tag as string */
-        imageTag: PropTypes.string.isRequired,
-      }).isRequired,
-      /** SKU seller */
-      seller: PropTypes.shape({
-        /** Seller comertial offer */
-        commertialOffer: PropTypes.shape({
-          /** SKU installments */
-          Installments: PropTypes.arrayOf(
-            PropTypes.shape({
-              /** Installment value */
-              Value: PropTypes.number.isRequired,
-              /** Interest rate (zero if interest-free) */
-              InterestRate: PropTypes.number.isRequired,
-              /** Calculated total value */
-              TotalValuePlusInterestRate: PropTypes.number,
-              /** Number of installments */
-              NumberOfInstallments: PropTypes.number.isRequired,
-              /** Installments offer name */
-              Name: PropTypes.string,
-            })
-          ),
-          /** Selling Price */
-          Price: PropTypes.number.isRequired,
-          /** List Price */
-          ListPrice: PropTypes.number.isRequired,
-        }).isRequired,
-      }).isRequired,
-    }).isRequired,
-  }),
-  /** Shows the product list price */
-  showListPrice: PropTypes.bool,
-  /** Set pricing labels' visibility */
-  showLabels: PropTypes.bool,
-  /** Set installments' visibility */
-  showInstallments: PropTypes.bool,
-  /** Set the discount badge's visibility */
-  showBadge: PropTypes.bool,
-  /** Text shown on badge */
-  badgeText: PropTypes.string,
-  /** Custom buy button text */
-  buyButtonText: PropTypes.string,
-  /** Hides the buy button completely . If active, the button will not be shown in any condition */
-  hideBuyButton: PropTypes.bool,
-  /** Defines if the button is shown only if the mouse is on the summary */
-  showButtonOnHover: PropTypes.bool,
-}
-
-ProductSummary.defaultProps = {
-  showListPrice: true,
-  showInstallments: true,
-  showLabels: true,
-  showBadge: true,
-  hideBuyButton: false,
-  showOnHover: false,
 }
 
 const defaultSchema = {
