d24e888de8216956899f47b7623e4877aa33bb68
Rerisson Daniel <rerisson.matos@splab.ufcg.edu.br>
2018-12-05 13:28:24 -0300

    Use product image according to client device

--- react/components/Image.js
+++ react/components/Image.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import { withRuntimeContext } from 'render'
+
+const MOBILE_SIZE = 200
+const DESKTOP_SIZE = 500
 
 /** Image component with 1:1 aspect ratio */
-export default function Image({ src, className }) {
-  return (
-    <div
-      className={classNames(className, 'vtex-aspect-ratio w-100')}
-      style={{ backgroundImage: `url(${src})` }}
-    />
-  )
+class Image extends Component {
+
+  resizeUrl = src => {
+    //Check of src is already in a size defined by the client
+    const resized = /[1-9]+-(:?([1-9]+)|(auto))/
+    if (resized.exec(src)) {
+      return src
+    }
+
+    const { runtime: { hints: { mobile } } } = this.props
+
+    const imageId = src.slice(src.indexOf('/ids/')).split('/')[2]
+    const idSuffix = imageId + (mobile ? `-${MOBILE_SIZE}-auto` : `-${DESKTOP_SIZE}-auto`)
+
+    return src.replace(imageId, idSuffix)
+  }
+
+  render() {
+    const { src, className } = this.props
+
+    return (
+      <div
+        className={classNames(className, 'vtex-aspect-ratio w-100')}
+        style={{ backgroundImage: `url(${this.resizeUrl(src)})` }}
+      />
+    )
+  }
 }
 
 Image.propTypes = {
@@ -18,3 +42,5 @@ Image.propTypes = {
   /** Custom classes */
   className: PropTypes.string,
 }
+
+export default withRuntimeContext(Image)
