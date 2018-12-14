0b4320bef02911b7220a4829cf8493d33ce63702
Bruno Dias <bruno.dias@vtex.com.br>
2018-12-14 16:10:20 -0300

    Add CSS Modules and Messages Builder

--- react/components/Image.js
+++ react/components/Image.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
+
+import productSummary from '../productSummary.css'
 
 /** Image component with 1:1 aspect ratio */
 export default function Image({ src, className }) {
   return (
     <div
-      className={classNames(className, 'vtex-aspect-ratio w-100')}
+      className={`${className} ${productSummary.aspectRatio} w-100`}
       style={{ backgroundImage: `url(${src})` }}
     />
   )
