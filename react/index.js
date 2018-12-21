53fda9854e315c9be4ff2b20342ad627a996dbaa
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2018-12-21 14:59:03 -0300

    Improve code quality

--- react/index.js
+++ react/index.js
@@ -174,21 +174,13 @@ class ProductSummary extends Component {
       'pv5': !showBorders
     })
 
-    const listPriceLabelClasses = classNames('dib strike t-small t-mini', {
-      't-mini': displayMode !== 'normal'
-    })
-
-    const listPriceClasses = classNames('dib ph2 strike t-small-ns t-mini', {
-      't-mini': displayMode === 'normal'
-    })
-
     return (
       <div className={containerClasses}>
         <ProductPrice
           className="flex flex-column justify-start"
           listPriceContainerClass="pv1 normal c-muted-2"
-          listPriceLabelClass={listPriceLabelClasses}
-          listPriceClass={listPriceClasses}
+          listPriceLabelClass="dib strike t-small t-mini"
+          listPriceClass="dib ph2 strike t-small-ns t-mini"
           sellingPriceContainerClass="pt1 pb3 c-on-base"
           sellingPriceLabelClass="dib"
           sellingPriceClass="dib ph2 6"
