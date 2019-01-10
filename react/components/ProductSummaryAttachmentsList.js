5aabf1ca3be802ff357780be43a110bd084d887d
Joao Fidelis <joaogfidelis@gmail.com>
2019-01-10 18:46:32 -0200

    handle parent price

--- react/components/ProductSummaryAttachmentsList.js
+++ react/components/ProductSummaryAttachmentsList.js
@@ -1,38 +1,63 @@
-import React, { Component } from 'react'
-import { path } from 'ramda'
+import React, { Component, Fragment } from 'react'
+import { intlShape, injectIntl } from 'react-intl'
 import ProductPrice from 'vtex.store-components/ProductPrice'
 import { productShape } from '../propTypes'
 
-export default class ProductSummaryAttachmentsList extends Component {
+class ProductSummaryAttachmentsList extends Component {
   static propTypes = {
     product: productShape,
+    intl: intlShape,
   }
 
-  hasPrice = (option) => path(['sku', 'seller', 'commertialOffer', 'Price'], option) > 0
+  // Achar maneira melhor de saber qual attachment nao mostrar
+  canShow = (option) => option.optionType !== 'Basic Toppings'
+
+  parentPrice = () => {
+    const { product } = this.props
+    const wholePrice = product.sku.seller.commertialOffer.Price
+    const parentPrice = product.addedOptions.reduce((total, { sku }) => 
+      total - sku.seller.commertialOffer.Price,
+      wholePrice
+      )
+    return parentPrice / product.quantity
+  }
+
+  renderItem = (productText, product, price) => {
+    return (
+      <div className="flex items-center justify-between" key={product.productName}>
+        <span className="f5 c-muted-2">{productText}</span>
+        <ProductPrice
+          sellingPrice={price}
+          sellingPriceContainerClass="pt1 pb3 c-on-base"
+          sellingPriceLabelClass="dib"
+          sellingPriceClass="dib f5 c-muted-2"
+          showListPrice={false}
+          showLabels={false}
+          showInstallments={false}
+        />
+      </div>
+    )
+  }
 
   render() {
-    const { product } = this.props
+    const { product, intl } = this.props
 
     if (!product.addedOptions || product.addedOptions.length === 0) {
       return null
     }
 
-    return product.addedOptions.filter(this.hasPrice).map(option => {
-      const productText = option.isSingleChoice ? option.productName : `+ ${option.quantity}× ${option.productName}`
-      return (
-        <div className="flex items-center justify-between" key={option.productName}>
-          <span className="f5 c-muted-2">{productText}</span>
-          <ProductPrice
-            sellingPrice={option.sku.seller.commertialOffer.Price * option.quantity}
-            sellingPriceContainerClass="pt1 pb3 c-on-base"
-            sellingPriceLabelClass="dib"
-            sellingPriceClass="dib f5 c-muted-2"
-            showListPrice={false}
-            showLabels={false}
-            showInstallments={false}
-          />
-        </div>
-      )
-    })
+    return (
+      <Fragment>
+        {this.renderItem(intl.formatMessage({ id: 'editor.productSummary.unit' }), product, this.parentPrice())}
+        {product.addedOptions.filter(this.canShow).map(option => {
+          const productText = 
+            option.isSingleChoice ? option.productName : `+ ${option.quantity / product.quantity}× ${option.productName}`
+          const price = option.sku.seller.commertialOffer.Price / product.quantity
+          return this.renderItem(productText, option, price)
+        })}
+      </Fragment> 
+    )
   }
 }
+
+export default injectIntl(ProductSummaryAttachmentsList)
