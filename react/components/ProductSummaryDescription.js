7fe1cdff7d07e8434c29ac747b9cf872f25487c5
Paulo Soares <paulo.soares@ccc.ufcg.edu.br>
2019-02-05 18:03:51 -0300

    Return emtpy div in case of no description provided

--- react/components/ProductSummaryDescription.js
+++ react/components/ProductSummaryDescription.js
@@ -5,6 +5,9 @@ const MAX_SIZE_DESCRIPTION = 120
 const ProductSummaryDescription = props => {
   const { descriptionClasses, description } = props
 
+  if (!description)
+    return (<div />)
+
   const descriptionTruncated = 
     description.length > MAX_SIZE_DESCRIPTION 
     ? `${description.substring(0, MAX_SIZE_DESCRIPTION)}...`
