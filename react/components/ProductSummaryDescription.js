5f7cd2a026f85ea2e5a8cd82c388e2764efbe8bc
Paulo Soares <paulo.soares@ccc.ufcg.edu.br>
2019-02-05 16:31:33 -0300

    Create ProductSummaryDescription

---
+++ react/components/ProductSummaryDescription.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const MAX_SIZE_DESCRIPTION = 120
+
+const ProductSummaryDescription = props => {
+  const { descriptionClasses, description } = props
+
+  const descriptionTruncated = 
+    description.length > MAX_SIZE_DESCRIPTION 
+    ? `${description.substring(0, MAX_SIZE_DESCRIPTION)}...`
+    : description
+
+  return (
+    <div className={descriptionClasses}>
+      {descriptionTruncated}
+    </div>
+  )
+}
+
+export default ProductSummaryDescription
