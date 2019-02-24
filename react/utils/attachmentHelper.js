43460675b9ce03b9fd6e32fc11053af7f6e87915
Joao Fidelis <joaogfidelis@gmail.com>
2019-02-24 18:24:24 -0300

    remove unused helper functions and its tests

--- react/utils/attachmentHelper.js
+++ react/utils/attachmentHelper.js
@@ -7,12 +7,3 @@ export const CHOICE_TYPES = {
 }
 
 export const getProductPrice = (product) => pathOr(0, ['sku', 'seller', 'commertialOffer', 'Price'], product)
-
-export const parentPricePerUnit = product => {
-  const wholePrice = getProductPrice(product)
-  const parentPrice = product.assemblyOptions.added.reduce((total, option) => 
-    total - option.item.sellingPrice / 100 * option.item.quantity,
-    wholePrice
-    )
-  return parentPrice / product.quantity
-}
