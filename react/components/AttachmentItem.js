5af2f86749349ccb82e9085126e974ea3a9912d3
Joao Fidelis <joaogfidelis@gmail.com>
2019-02-27 14:00:13 -0300

    add css classes

--- react/components/AttachmentItem.js
+++ react/components/AttachmentItem.js
@@ -2,12 +2,14 @@ import React, { memo } from 'react'
 import { string, number } from 'prop-types'
 import ProductPrice from 'vtex.store-components/ProductPrice'
 
+import styles from '../productSummary.css'
+
 const AttachmentItem = ({
   productText,
   price
 }) => {
   return (
-    <div className="flex items-center justify-between pv1" key={productText}>
+    <div className={`${styles.attachmentItemContainer} flex items-center justify-between pv1`} key={productText}>
       <span className="t-small c-muted-2 tl pr3">{productText}</span>
       {price != null && (
         <ProductPrice
