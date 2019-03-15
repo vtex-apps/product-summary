5cf0c0fc46eda06e6cec575488fafcf9265094d0
Theo Moura <theomouram@gmail.com>
2019-03-15 14:23:22 -0300

    Fix mocks

--- react/__mocks__/vtex.store-components/BuyButton.js
+++ react/__mocks__/vtex.store-components/BuyButton.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 /**
  * BuyButton Mocked Component.
  */
-const BuyButton = ({ children, className }) => {
-  return <div className={`buy-button-mock ${className}`}>{children}</div>
+const BuyButton = ({ children }) => {
+  return <div className="buy-button-mock">{children}</div>
 }
 
 export default BuyButton
