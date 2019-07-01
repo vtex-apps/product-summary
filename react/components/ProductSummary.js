efce75db66ce1f190bab049354f7701d2ae7bcf0
Tiago Gimenes <tlgimenes@gmail.com>
2019-07-01 15:11:19 -0300

    link with product id

--- react/components/ProductSummary.js
+++ react/components/ProductSummary.js
@@ -65,7 +65,10 @@ class ProductSummaryCustom extends Component {
           <Link
             className={`${productSummary.clearLink} h-100 flex flex-column`}
             page={'store.product'}
-            params={{ slug: path(['linkText'], product) }}
+            params={{
+              slug: product && product.linkText, 
+              id: product && product.productId
+            }}
             onClick={actionOnClick}
           >
             <article className={summaryClasses}>
