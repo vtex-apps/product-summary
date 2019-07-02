a78f5de94cab53f82a4d0795ca06c1872945dc24
Breno Calazans <brenocalazans@gmail.com>
2019-07-02 13:41:23 -0300

    Improve message

--- react/components/ProductSummaryContext.js
+++ react/components/ProductSummaryContext.js
@@ -4,7 +4,7 @@ const ProductSummaryContext = createContext(0)
 
 const OriginalConsumer = ProductSummaryContext.Consumer
 ProductSummaryContext.Consumer = function WrappedConsumer(props) {
-  console.error('If you are seeing this, please comment on the issue https://github.com/vtex-apps/store-issues#28')
+  console.error('If you are seeing this, a component it\'s using ProductSummaryContext from vtex.product-summary, which is deprecated. Please see the issue https://github.com/vtex-apps/store-issues#28 on how to migrate to the new component.')
 
   return <OriginalConsumer {...props} />
 }
