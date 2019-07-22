f2d80ccef6b9dca7d2d52008801365fdd2ec747c
Breno Calazans <brenocalazans@gmail.com>
2019-07-22 16:56:52 -0300

    Add mocked function

--- react/__mocks__/vtex.store-components/BuyButton.js
+++ react/__mocks__/vtex.store-components/BuyButton.js
@@ -7,4 +7,26 @@ const BuyButton = ({ children }) => {
   return <div className="buy-button-mock">{children}</div>
 }
 
+BuyButton.mapCatalogItemToCart = function mapCatalogItemToCart({
+  selectedQuantity,
+}) {
+  return [
+    {
+      index: 0,
+      quantity: selectedQuantity,
+      detailUrl: '/product-name/p',
+      name: 'Product name',
+      brand: 'Brand',
+      category: 'Category',
+      productRefId: 'REF',
+      seller: 'VTEX',
+      price: 9,
+      listPrice: 10,
+      variant: 'Name',
+      skuId: '1',
+      imageUrl: 'https://foo.com/image.png',
+    },
+  ]
+}
+
 export default BuyButton
