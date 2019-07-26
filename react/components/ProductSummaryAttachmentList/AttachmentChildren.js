e287037fffbdb2deebd063e21f1c8e7a6fe4b382
Joao Fidelis <joaogfidelis@gmail.com>
2019-07-26 10:20:18 -0300

    add css class

--- react/components/ProductSummaryAttachmentList/AttachmentChildren.js
+++ react/components/ProductSummaryAttachmentList/AttachmentChildren.js
@@ -2,13 +2,15 @@ import React from 'react'
 import AddedAttachmentsList from './AddedAttachmentsList'
 import RemovedAttachmentsList from './RemovedAttachmentsList'
 
+import styles from '../../productSummary.css'
+
 const AttachmentChildren = ({ addedOptions, removedOptions }) => {
   if (addedOptions.length === 0 && removedOptions.length === 0) {
     return null
   }
 
   return (
-    <div className="childrenContainer ml3">
+    <div className={`${styles.attachmentChildrenContainer} ml3`}>
       <AddedAttachmentsList addedOptions={addedOptions} showItemPrice={false} />
       <RemovedAttachmentsList removedOptions={removedOptions} />
     </div>
