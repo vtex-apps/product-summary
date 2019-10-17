da966a35f3a71399fdb95397abd5d6251e11a7c3
victorhmp <victor2142@gmail.com>
2019-10-17 17:28:11 -0300

    Add schema to enable edition via Site Editor

--- react/ProductSummaryList.js
+++ react/ProductSummaryList.js
@@ -44,35 +44,35 @@ const parseFilters = ({ id, value }) => `specificationFilter_${id}:${value}`
 
 const ORDER_BY_OPTIONS = {
   ORDER_BY_RELEVANCE: {
-    name: 'admin/editor.shelf.orderType.relevance',
+    name: 'admin/editor.productSummaryList.orderType.relevance',
     value: '',
   },
   ORDER_BY_TOP_SALE_DESC: {
-    name: 'admin/editor.shelf.orderType.sales',
+    name: 'admin/editor.productSummaryList.orderType.sales',
     value: 'OrderByTopSaleDESC',
   },
   ORDER_BY_PRICE_DESC: {
-    name: 'admin/editor.shelf.orderType.priceDesc',
+    name: 'admin/editor.productSummaryList.orderType.priceDesc',
     value: 'OrderByPriceDESC',
   },
   ORDER_BY_PRICE_ASC: {
-    name: 'admin/editor.shelf.orderType.priceAsc',
+    name: 'admin/editor.productSummaryList.orderType.priceAsc',
     value: 'OrderByPriceASC',
   },
   ORDER_BY_NAME_ASC: {
-    name: 'admin/editor.shelf.orderType.nameAsc',
+    name: 'admin/editor.productSummaryList.orderType.nameAsc',
     value: 'OrderByNameASC',
   },
   ORDER_BY_NAME_DESC: {
-    name: 'admin/editor.shelf.orderType.nameDesc',
+    name: 'admin/editor.productSummaryList.orderType.nameDesc',
     value: 'OrderByNameDESC',
   },
   ORDER_BY_RELEASE_DATE_DESC: {
-    name: 'admin/editor.shelf.orderType.releaseDate',
+    name: 'admin/editor.productSummaryList.orderType.releaseDate',
     value: 'OrderByReleaseDateDESC',
   },
   ORDER_BY_BEST_DISCOUNT_DESC: {
-    name: 'admin/editor.shelf.orderType.discount',
+    name: 'admin/editor.productSummaryList.orderType.discount',
     value: 'OrderByBestDiscountDESC',
   },
 }
@@ -84,6 +84,7 @@ const productQueryOptions = {
     hideUnavailableItems = false,
     orderBy = ORDER_BY_OPTIONS.ORDER_BY_TOP_SALE_DESC.value,
     specificationFilters = [],
+    maxItems = 10,
   }) => ({
     ssr: true,
     name: 'productList',
@@ -97,15 +98,91 @@ const productQueryOptions = {
       specificationFilters: specificationFilters.map(parseFilters),
       orderBy,
       from: 0,
-      to: 9,
+      to: maxItems - 1,
       hideUnavailableItems,
     },
   }),
 }
 
+function getOrdenationNames() {
+  const names = []
+  for (const key in ORDER_BY_OPTIONS) {
+    names.push(ORDER_BY_OPTIONS[key].name)
+  }
+  return names
+}
+
+function getOrdenationValues() {
+  const values = []
+  for (const key in ORDER_BY_OPTIONS) {
+    values.push(ORDER_BY_OPTIONS[key].value)
+  }
+  return values
+}
+
 const EnhancedProductList = compose(
   graphql(productsQuery, productQueryOptions)
 )(ProductSummaryList)
+
+EnhancedProductList.getSchema = () => ({
+  title: 'admin/editor.productSummaryList.title',
+  description: 'admin/editor.productSummaryList.description',
+  type: 'object',
+  properties: {
+    category: {
+      title: 'admin/editor.productSummaryList.category.title',
+      description: 'admin/editor.productSummaryList.category.description',
+      type: 'string',
+      isLayout: false,
+    },
+    specificationFilters: {
+      title: 'admin/editor.productSummaryList.specificationFilters.title',
+      type: 'array',
+      items: {
+        title:
+          'admin/editor.productSummaryList.specificationFilters.item.title',
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            title:
+              'admin/editor.productSummaryList.specificationFilters.item.id.title',
+          },
+          value: {
+            type: 'string',
+            title:
+              'admin/editor.productSummaryList.specificationFilters.item.value.title',
+          },
+        },
+      },
+    },
+    collection: {
+      title: 'admin/editor.productSummaryList.collection.title',
+      type: 'number',
+      isLayout: false,
+    },
+    orderBy: {
+      title: 'admin/editor.productSummaryList.orderBy.title',
+      type: 'string',
+      enum: getOrdenationValues(),
+      enumNames: getOrdenationNames(),
+      default: ORDER_BY_OPTIONS.ORDER_BY_TOP_SALE_DESC.value,
+      isLayout: false,
+    },
+    hideUnavailableItems: {
+      title: 'admin/editor.productSummaryList.hideUnavailableItems',
+      type: 'boolean',
+      default: false,
+      isLayout: false,
+    },
+    maxItems: {
+      title: 'admin/editor.productSummaryList.maxItems.title',
+      type: 'number',
+      isLayout: false,
+      default: 10,
+    },
+  },
+})
 
 export default EnhancedProductList
 
