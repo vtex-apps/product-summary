3d0da91884d6f5516730644d44807fc4d50c1e91
iaron <iaron.araujo@vtex.com.br>
2019-10-30 17:04:00 -0300

    fix

--- react/Column.js
+++ react/Column.js
@@ -5,7 +5,7 @@ const CSS_HANDLES = ['column']
 
 const Column = ({ children }) => {
   const handles = useCssHandles(CSS_HANDLES)
-  return <div className={`${handles.column}`}>{children}</div>
+  return <div className={`${handles.column} flex items-center`}>{children}</div>
 }
 
 export default Column
