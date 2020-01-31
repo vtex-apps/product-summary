f7a8b76f4b3f347427f4eae9ffc65f1789d01ce4
victorhmp <victor2142@gmail.com>
2020-01-31 17:13:57 -0300

    Update implementation of ProductSummaryList

--- react/ProductSummaryListChildren.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { ListContextProvider } from 'vtex.list-context'
-
-const ProductSummaryListChildren = ({ children, newList }) => (
-  <ListContextProvider list={newList}>{children}</ListContextProvider>
-)
-
-export default ProductSummaryListChildren
