b5fc74d7b474e1e60bca8c0fdee0b619942f10d9
Hiago Lucas Cardeal <hcardealdemelo@gmail.com>
2020-09-17 18:51:13 -0300

    add searchpage condition

--- react/hooks/useIsPriceAsync.js
+++ react/hooks/useIsPriceAsync.js
@@ -1,7 +1,13 @@
 import { useSearchPage } from 'vtex.search-page-context/SearchPageContext'
 
 const useIsPriceAsync = () => {
-  const { searchQuery } = useSearchPage()
+  const searchPage = useSearchPage()
+
+  if (!searchPage) {
+    return false
+  }
+
+  const { searchQuery } = searchPage
 
   return {
     isPriceAsync: searchQuery?.variables?.simulationBehavior === 'async',
