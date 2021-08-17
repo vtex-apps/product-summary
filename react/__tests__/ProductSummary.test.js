2801191c0a56c4859494a031bcf061e66c9858d8
Santiago Sanz <ssaannzz@hotmail.com>
2021-08-17 11:40:11 +0100

    feat: rewrote tests

--- react/__tests__/ProductSummary.test.js
+++ react/__tests__/ProductSummary.test.js
@@ -1,8 +1,12 @@
 import ProductSummary from '../ProductSummaryCustom'
 
 describe('<ProductSummary /> component', () => {
-  it(`should parse a product succesfully and get sku as the first with available quantity`, () => {
-    const product = {
+  it(`should parse a catalog product and get an available SKU with the highest price`, () => {
+    const inheritedProps = {
+      preferredSKU: 'PRICE_ASC',
+    }
+
+    const mockProduct = {
       productId: '123456789',
       linkText: 'linkText',
       productName: 'productName',
@@ -15,7 +19,7 @@ describe('<ProductSummary /> component', () => {
               sellerId: '1',
               commertialOffer: {
                 AvailableQuantity: 0,
-                Price: 10,
+                Price: 100,
               },
             },
           ],
@@ -38,7 +42,30 @@ describe('<ProductSummary /> component', () => {
               sellerId: '1',
               commertialOffer: {
                 AvailableQuantity: 3,
-                Price: 15,
+                Price: 50,
+              },
+            },
+          ],
+          images: [
+            {
+              imageUrl: 'image1',
+              imageLabel: null,
+            },
+            {
+              imageUrl: 'image2',
+              imageLabel: null,
+            },
+          ],
+        },
+        {
+          itemId: '3',
+          name: 'item 3',
+          sellers: [
+            {
+              sellerId: '1',
+              commertialOffer: {
+                AvailableQuantity: 1,
+                Price: 500,
               },
             },
           ],
@@ -59,14 +86,114 @@ describe('<ProductSummary /> component', () => {
           name: 'name',
         },
       ],
-      quantity: 1,
+      properties: [],
     }
 
-    const result = ProductSummary.mapCatalogProductToProductSummary(product)
+    const result = ProductSummary.mapCatalogProductToProductSummary(
+      mockProduct,
+      inheritedProps.preferredSKU
+    )
 
     expect(result).toBeDefined()
     expect(result.sku.seller.sellerId).toBe('1')
     expect(result.sku.image).toBeDefined()
+    expect(result.sku.itemId).toBe('3')
+  })
+
+  it(`should parse a catalog product and get a specific SKU by it's properties`, () => {
+    const inheritedProps = {
+      preferredSKU: 'LAST_AVAILABLE',
+    }
+
+    const mockProduct = {
+      productId: '123456789',
+      linkText: 'linkText',
+      productName: 'productName',
+      items: [
+        {
+          itemId: '1',
+          name: 'item 1',
+          sellers: [
+            {
+              sellerId: '1',
+              commertialOffer: {
+                AvailableQuantity: 0,
+                Price: 100,
+              },
+            },
+          ],
+          images: [
+            {
+              imageUrl: 'image1',
+              imageLabel: null,
+            },
+            {
+              imageUrl: 'image2',
+              imageLabel: null,
+            },
+          ],
+        },
+        {
+          itemId: '2',
+          name: 'item 2',
+          sellers: [
+            {
+              sellerId: '1',
+              commertialOffer: {
+                AvailableQuantity: 3,
+                Price: 50,
+              },
+            },
+          ],
+          images: [
+            {
+              imageUrl: 'image1',
+              imageLabel: null,
+            },
+            {
+              imageUrl: 'image2',
+              imageLabel: null,
+            },
+          ],
+        },
+        {
+          itemId: '3',
+          name: 'item 3',
+          sellers: [
+            {
+              sellerId: '1',
+              commertialOffer: {
+                AvailableQuantity: 1,
+                Price: 500,
+              },
+            },
+          ],
+          images: [
+            {
+              imageUrl: 'image1',
+              imageLabel: null,
+            },
+            {
+              imageUrl: 'image2',
+              imageLabel: null,
+            },
+          ],
+        },
+      ],
+      productClusters: [
+        {
+          name: 'name',
+        },
+      ],
+      properties: [{ name: 'DefaultSKUSelected', values: ['2'] }],
+    }
+
+    const result = ProductSummary.mapCatalogProductToProductSummary(
+      mockProduct,
+      inheritedProps.preferredSKU
+    )
+
+    expect(result).toBeDefined()
     expect(result.sku.itemId).toBe('2')
   })
 
