ad598f202fd437a702c7d018cafde44de0f19d55
Kai Brockelt <93577143+kaibrockelt@users.noreply.github.com>
2021-12-10 19:01:29 +0100

    Update ProductSummaryCategoryLink.tsx
    lint

--- react/ProductSummaryCategoryLink.tsx
+++ react/ProductSummaryCategoryLink.tsx
@@ -2,28 +2,27 @@ import React from 'react'
 import { ProductSummaryContext } from 'vtex.product-summary-context'
 import { useCssHandles } from 'vtex.css-handles'
 import type { CssHandlesTypes } from 'vtex.css-handles'
-import styles from './productSummary.css'
 
+import styles from './productSummary.css'
 
 const { useProductSummary } = ProductSummaryContext
 
 const CSS_HANDLES = [
   'categoryLink',
   'categoryLinkContainer',
-  'loading' ] as const
+  'loading',
+] as const
 
 interface Props {
   classes: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
 }
 
 
-function ProductSummaryCategoryLink({
-  classes,
-}: Props) {
+function ProductSummaryCategoryLink( { classes } : Props) {
   const { product, isLoading } = useProductSummary()
   const { handles } = useCssHandles(CSS_HANDLES, { classes })
-  const mylink = buildLink(product)
-  if (isLoading ) {
+  const mylink = buildLink()
+  if (isLoading) {
     return (
       <div
         className={`${handles.loading} flex items-end justify-end w-100 h1 pr6`}
@@ -32,25 +31,26 @@ function ProductSummaryCategoryLink({
       </div>
     )
   }
-  
-  function blocklink(e: any){
-    e.stopPropagation();
+
+  function blocklink(e: any) {
+    e.stopPropagation()
   }
 
-  function buildLink(product: any){
-    const categories = product?.categories[0];
-    let lastcat = categories.split("/");
-    const url= categories.toLowerCase().replace(/[ &]/g, '-');
-    lastcat=lastcat[lastcat.length-2];
-    return (<a href={url} className={handles.categoryLink}>{lastcat}</a>)
+  function buildLink() {
+    const categories = product?.categories[0]
+    let lastcat = categories.split('/');
+    const url = categories.toLowerCase().replace(/[ &]/g, '-');
+    lastcat = lastcat[lastcat.length-2];
+    return (
+      ······<a href={url} className={handles.categoryLink}>{lastcat}</a>
+    )
   }
-
 
   return (
-    <div className={handles.categoryLinkContainer} onClick={blocklink}>
-        {mylink}
+    <div role="link" onKeyPress={blocklink} className={handles.categoryLinkContainer} onClick={blocklink}>
+      {mylink}
     </div>
   )
 }
-  
+
 export default ProductSummaryCategoryLink
