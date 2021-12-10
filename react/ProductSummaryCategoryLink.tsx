e08fa0c12aa6eb7cccb25962d0c37910ae4a35a7
Kai Brockelt <93577143+kaibrockelt@users.noreply.github.com>
2021-12-10 19:49:32 +0100

    Update ProductSummaryCategoryLink.tsx
    .

--- react/ProductSummaryCategoryLink.tsx
+++ react/ProductSummaryCategoryLink.tsx
@@ -17,8 +17,7 @@ interface Props {
   classes: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
 }
 
-
-function ProductSummaryCategoryLink( { classes } : Props) {
+function ProductSummaryCategoryLink({ classes } : Props) {
   const { product, isLoading } = useProductSummary()
   const { handles } = useCssHandles(CSS_HANDLES, { classes })
   const mylink = buildLink(product)
