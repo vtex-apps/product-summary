e27ecec2c5ae42a9ae75a4013ffb50d5ea67b76f
Artur Magalhães <nezkal@gmail.com>
2023-08-03 14:28:11 -0300

    feat: Added a prop to set the max description dynamically (#380)
    #### What problem is this solving?
    
    It has been added a way to set the max description which is hardcoded by
    a constant (120).
    
    This way is possible to set a new property on the **Product Description
    Component** which makes it possible to set it on the theme blocks.
    
    #### How to test it?
    
    https://artur2--beautycounterqa.myvtex.com/
    
    #### Screenshots or example usage:
    
    <img width="578" alt="CleanShot 2023-08-02 at 18 05 52@2x"
    src="https://github.com/vtex-apps/product-summary/assets/5812946/99942380-041a-4711-ae1e-765e0d229ce6">

--- react/ProductSummaryDescription.tsx
+++ react/ProductSummaryDescription.tsx
@@ -11,9 +11,10 @@ const CSS_HANDLES = ['description'] as const
 
 interface Props {
   classes?: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
+  descriptionMaxSize?: number
 }
 
-function ProductSummaryDescription({ classes }: Props) {
+function ProductSummaryDescription({ classes, descriptionMaxSize }: Props) {
   const { handles } = useCssHandles(CSS_HANDLES, { classes })
 
   const {
@@ -26,9 +27,10 @@ function ProductSummaryDescription({ classes }: Props) {
 
   const descriptionClasses = `${handles.description} c-muted-2 t-small`
 
+  const maxSize = descriptionMaxSize ?? MAX_SIZE_DESCRIPTION
   const descriptionTruncated =
-    description.length > MAX_SIZE_DESCRIPTION
-      ? `${description.substring(0, MAX_SIZE_DESCRIPTION)}...`
+    description.length > maxSize
+      ? `${description.substring(0, maxSize)}...`
       : description
 
   return (
