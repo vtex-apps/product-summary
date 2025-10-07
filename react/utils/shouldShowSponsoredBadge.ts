d6313bc7d499093ed2b873154c5619111d5bea4b
Rodrigo Orem <rodrigo.orem@vtex.com>
2025-10-07 12:21:20 -0300

    feat: use the new ad event parameter

--- react/utils/shouldShowSponsoredBadge.ts
+++ react/utils/shouldShowSponsoredBadge.ts
@@ -3,12 +3,16 @@ import {
   SponsoredBadgePosition,
 } from 'vtex.product-summary-context/react/ProductSummaryTypes'
 
+interface NewAdvertisement {
+  eventParameters?: string
+}
+
 const shouldShowSponsoredBadge = (
   product: Product,
   position: SponsoredBadgePosition,
   eligiblePosition: SponsoredBadgePosition
 ) => {
-  const isSponsored = !!product?.advertisement?.adId
+  const isSponsored = !!product?.advertisement?.adId || !!(product.advertisement as NewAdvertisement)?.eventParameters
   const showSponsoredBadge = isSponsored && position === eligiblePosition
 
   return showSponsoredBadge
